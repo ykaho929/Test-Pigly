84fd0e4c6cb0375aee2c0ab0256b578625f24c70
ykaho <h12.929.kaho@gmail.com>
2025-01-11 13:58:19 +0900

    DB追加ディレクトリ作成

--- src/database/migrations/2025_01_11_123857_create_weight_target_table.php
+++ src/database/migrations/2025_01_11_123857_create_weight_target_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWeightTargetTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('weight_target', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+            $table->integer('user_id');
+            $table->decimal('target_weight', $precison=4 , $scale=1);
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('weight_target');
     }
 }
